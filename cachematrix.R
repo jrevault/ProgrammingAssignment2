51899fffad2b0941cb93b0b607d9a385c0523328
jrevault <jrevault@gmail.com>
2014-05-20 16:36:47 +0200

    Functionnal version

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function caches the inverse of a matrix.
+## It takes a matrix as an arguments
+## and offers function to 'get' and 'set' the
+## solved matrix of the given matrix
+makeCacheMatrix <- function( x = matrix() ) {
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+  # We initiate the solved matrix to a NULL
+  sm <- NULL
+  # We declare a function that caches into a different environment
+  # Setter function for the original  matrix
+  set <- function( y ) {
+    x <<- y
+    sm <<- NULL
+  }
+  # Getter function for the original matrix
+  get <- function() x
+  # Setter function  for the solved matrix
+  setinverse <- function( solve ) sm <<- solve
+  # Getter function for the solved matrix
+  getinverse <- function() sm
+  # Prepares the abilities over the current object
+  list( set = set, 
+        get = get,
+        setinverse = setinverse,
+        getinverse = getinverse )
+  
 }
 
-
-## Write a short comment describing this function
-
+## This function returns th inverse of a square invertible matrix.
+## It takes a matrix as an arguments, as well as all the arguments the
+## 'solve' function could take
+## This function calls the 'getinverse' solved matrix on the current matrix
+## in order to get it if it has already bean computed
+## if it hasn't, it solves the matrix and save it into cache via th 
+## 'setinverse' function. After that it just returns the solved matrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  # Gets the matrix that is the inverse of 'x' if it was already computed
+  sm <- x$getinverse()
+  # If we have an inverse matrix in cache
+  if( ! is.null( sm ) ) {
+    # Print message
+    message( "getting cached data" )
+    # returns the solved matrix from cache
+    return( sm )
+  }
+  # Otherwise we get the data
+  data <- x$get()
+  # then compute the inverse of the matrix
+  sm <- solve( data , ... )
+  # Store the inverse matrix inside cache
+  x$setinverse( sm )
+  # And return the newly computed solved matrix
+  sm
 }
